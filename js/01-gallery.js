8b3d2ee7e357933240cfad94da1dc32d26259847
Olehys <arzamazova13@gmail.com>
2022-11-05 13:14:58 +0200

    task 01

---
+++ js/01-gallery.js
@@ -0,0 +1,57 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+function createGaleryImg(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      let toggleUrl = preview;
+      return `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${toggleUrl}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`;
+    })
+    .join("");
+}
+
+function blockStandartAction(evt) {
+  evt.preventDefault();
+
+  if (evt.target.nodeName !== "IMG") {
+    console.log("this not IMG");
+    return;
+  }
+}
+
+function onShowPicture(evt) {
+  blockStandartAction(evt);
+  // open
+
+  const instance = basicLightbox.create(`  
+   <img src="${evt.target.getAttribute(
+     "data-source"
+   )}" width="800" height="600">
+  `);
+  instance.show();
+  // close
+
+  galleryEl.addEventListener("keydown", (evt) => {
+    if (evt.code === "Escape") {
+      instance.close();
+    }
+  });
+}
+
+// show event
+const galleryEl = document.querySelector(".gallery");
+
+galleryEl.addEventListener("click", onShowPicture);
+
+// markup
+const imgMarkup = createGaleryImg(galleryItems);
+galleryEl.insertAdjacentHTML("beforeend", imgMarkup);
