3bb5837a607fb82bfd7eedd99842bb05b7c1493b
Olehys <arzamazova13@gmail.com>
2022-11-05 13:54:16 +0200

    task 02

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,7 +4,6 @@ import { galleryItems } from "./gallery-items.js";
 function createGaleryImg(images) {
   return images
     .map(({ preview, original, description }) => {
-      let toggleUrl = preview;
       return `<div class="gallery__item">
         <a class="gallery__link" href="${original}">
           <img
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,38 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+function createGaleryImg(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" title="${description}" />
+</a>`;
+    })
+    .join("");
+}
+
+function blockStandartAction(evt) {
+  evt.preventDefault();
+
+  if (evt.target.nodeName !== "IMG") {
+    console.log("this not IMG");
+    return;
+  }
+}
+
+function onShowPicture(evt) {
+  blockStandartAction(evt);
+  // open
+  var lightbox = new SimpleLightbox(".gallery a", {});
+}
+
+// show event
+const galleryEl = document.querySelector(".gallery");
+
+galleryEl.addEventListener("click", onShowPicture);
+
+// markup
+const imgMarkup = createGaleryImg(galleryItems);
+galleryEl.insertAdjacentHTML("beforeend", imgMarkup);
